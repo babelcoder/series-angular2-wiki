93bcae9f4e30a5e1a940833986246eced4529461
n_thongjor <n.thongjor@gmail.com>
2016-08-28 00:12:30 +0700

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
+import { ApplicationRef, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { HeaderComponent } from './shared';
+
+// Barrels are not reaching into sub barrels: https://github.com/angular/angular-cli/issues/585
+// import {
+//   AppComponent,
+//   HeaderComponent,
+//   HomeComponent
+// } from './'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent, 
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,5 @@
 export * from './environments/environment';
 export * from './app.component';
 export * from './app.module';
+export * from './home';
+export * from './shared';
